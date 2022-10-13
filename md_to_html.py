c660805797702374a4f95d7b41066a1b7af7658a
Tim Saylor <tim.saylor@gmail.com>
2022-10-13 12:19:03 -0500

    add indexes

--- md_to_html.py
+++ md_to_html.py
@@ -9,15 +9,24 @@ tpl = env.get_template('page.html')
 md_root = Path('md')
 top_level_dirs = [p.name for p in md_root.iterdir() if p.is_dir()]
 
-for inpath in md_root.glob('**/*.md'):
+inpaths = list(md_root.glob('**/*.md'))
+for inpath in inpaths:
     outpath = Path('build') / inpath.relative_to('md').with_suffix('.html')
     os.makedirs(outpath.parent, exist_ok=True)
+    if inpath.name == "index.md":
+        sublinks = [
+            (p.relative_to('md').with_suffix('.html'), p.with_suffix('').name)
+            for p in inpaths if p.parent == inpath.parent and p.name != "index.md"
+        ]
+    else:
+        sublinks = []
     with open(inpath) as infile, open(outpath, 'w') as outfile:
         md_content = infile.read()
         html_content = markdown.markdown(md_content, output_format='html5')
         outfile.write(
             tpl.render(
                 links=top_level_dirs,
-                content=html_content
+                content=html_content,
+                sublinks=sublinks
             )
         )
--- site.sh
+++ site.sh
@@ -2,13 +2,13 @@
 # https://www.grymoire.com/Unix/Sh.html
 case $1 in
     build | b )
-        docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll:latest jekyll build
+        python md_to_html.py
         ;;
     serve | server | s )
-        docker run --rm --volume="$PWD:/srv/jekyll" --publish 4000:4000 jekyll/jekyll jekyll serve
+        python -m http.server --directory=build
         ;;
-    shell | sh )
-        docker run --rm -it --volume="$PWD:/srv/jekyll" --publish 4000:4000 jekyll/jekyll /bin/bash
+    clean | c )
+        rm -rf build
         ;;
     * )
         echo "unrecognized command"
